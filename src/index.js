b31d2ead3b3d0c2f94059df799a3fba852bde318
tranduygioi1 <tranduygioi1108@gmail.com>
2023-07-05 23:11:40 +0700

    init project

--- src/index.js
+++ src/index.js
@@ -5,19 +5,26 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-//htttp logger
+
+  //htttp logger
 app.use(morgan('combined'));
 
+
 //template engine 
-//app.engine('handlebars', handlebars());
-//app.set('view engine', 'handlebars');
-//app.set('views', path.join(__dirname, 'resources/views'));
+app.engine('hbs', handlebars.engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'));
 
-console.log('PATH: ', path.join(__dirname, 'resources/views'))
 
-app.get('/tin-tuc', (req, res) => {
+app.get('/', (req, res) => {
    return res.render('home');
 })
+
+
+app.get('/news', (req, res) => {
+  return res.render('news');
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
